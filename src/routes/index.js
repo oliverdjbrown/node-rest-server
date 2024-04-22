ac2dcf0fedeba8f6b913c9210981f7b435ec883a
Oliver Brown <oliverbrodev@gmail.com>
2024-04-22 07:30:07 -0400

    Refactor file paths and import statements, update dependencies, and fix import in index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const { status404 } = require("../../constants/messages/http-responses");
+const { status404 } = require("../../constants");
 
 const pathRouter = `${__dirname}`;
 
